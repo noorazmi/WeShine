12c4a0a6468361e104d2f151a592d61df588d578
noorazmi <noorazmi@rediff.com>
2014-11-11 00:48:30 +0530

    files added

--- src/com/game/framents/MazeGame1Fragment.java
+++ src/com/game/framents/MazeGame1Fragment.java
@@ -4,10 +4,12 @@ import com.game.weshine.R;
 import com.game.weshine.R.layout;
 
 import android.app.Fragment;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 public class MazeGame1Fragment  extends Fragment{
 	
@@ -21,6 +23,12 @@ public class MazeGame1Fragment  extends Fragment{
      public View onCreateView(LayoutInflater inflater, ViewGroup container,
              Bundle savedInstanceState) {
          View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+         ImageView mBirdsView = (ImageView) rootView.findViewById(R.id.birdsView);
+         mBirdsView.setBackgroundResource(R.drawable.birds_animation);
+         AnimationDrawable birdsAnimation;
+         
+         birdsAnimation = (AnimationDrawable) mBirdsView.getBackground();
+         birdsAnimation.start();
          return rootView;
      }
 
